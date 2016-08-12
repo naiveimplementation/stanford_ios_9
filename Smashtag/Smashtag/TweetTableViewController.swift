832a27edad8537f00fa61771973541dc9e6fa722
pradeep <naiveimplementation@gmail.com>
2016-08-12 15:13:40 -0500

    end of lecture 9

---
+++ Smashtag/Smashtag/TweetTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by Pradeep Kumar on 8/11/16.
+//  Copyright © 2016 Pradeep. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewController: UITableViewController, UITextFieldDelegate {
+
+    var tweets = [Array<Twitter.Tweet>]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    var searchText: String? {
+        didSet {
+            tweets.removeAll()
+            searchForTweets()
+            title = searchText
+        }
+    }
+    
+    private var twitterRequest: Twitter.Request? {
+        if let query = searchText where !query.isEmpty {
+            return Twitter.Request(search: query + " -filter:retweets", count: 100)
+        }
+        return nil
+    }
+    
+    private var lastTwitterRequest: Twitter.Request?
+    
+    private func searchForTweets() {
+        if let request = twitterRequest {
+            lastTwitterRequest = request
+            request.fetchTweets { [weak weakSelf = self] newTweets in
+                dispatch_async(dispatch_get_main_queue()) {
+                    if request == weakSelf?.lastTwitterRequest {
+                        if !newTweets.isEmpty {
+                            weakSelf?.tweets.insert(newTweets, atIndex: 0)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = tableView.rowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    // MARK: - UITableViewDataSource
+
+    private struct Storyboard {
+        static let TweetCellIdentifier = "Tweet"
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return tweets.count
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tweets[section].count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.TweetCellIdentifier, forIndexPath: indexPath)
+
+        // Configure the cell...
+        let tweet = tweets[indexPath.section][indexPath.row]
+        if let tweetCell = cell as? TweetTableViewCell {
+            tweetCell.tweet = tweet
+        }
+        
+        return cell
+    }
+    
+    @IBOutlet weak var searchTextField: UITextField! {
+        didSet {
+            searchTextField.delegate = self
+            searchTextField.text = searchText
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        searchText = textField.text
+        return true
+    }
+}
